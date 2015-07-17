0c2a8a2c3b5cb554c296f6ab35a8bdbb8fa542e3
Kitzo <absuser@FrontCounter.ABS.local>
2015-07-17 15:20:47 -0400

    New Update

--- wp-content/themes/single-page-theme/page.php
+++ wp-content/themes/single-page-theme/page.php
@@ -41,7 +41,7 @@
             <?php $page_query = new WP_Query('post_type=page&post_parent='.$post->ID.'&order=ASC&orderby=menu_order'); ?>
             
             <?php while ($page_query->have_posts()) : $page_query->the_post(); ?>
-                    <div class="col-xs-12 col-sm-12 col-md-<?php echo 12/$cntPerRow; ?> col-lg-<?php echo 12/$cntPerRow; ?>">
+                    <div class="col-xs-12 col-sm-12 col-md-<?php echo 12/$cntPerRow; ?> col-lg-<?php echo 12/$cntPerRow; ?> center">
                                 <?php if ( has_post_thumbnail() ) { ?>
                                       <?php the_post_thumbnail(array(200,200), array('class'=>'img-thumbnail home-tile')); ?>
                                 <?php } ?>
